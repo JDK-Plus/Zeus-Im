c03a5aede6ff0bbdc9f8375ddd825b6cfe152445
高鹏飞 <pengfei51@staff.weibo.com>
2022-11-08 23:27:40 +0800

    init common、passport

---
+++ zeus-message/src/main/java/plus/jdk/zeus/message/ZeusMessageService.java
@@ -0,0 +1,12 @@
+package plus.jdk.zeus.message;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusMessageService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusMessageService.class, args);
+    }
+}
+
